9a06be9b3ed361b95d2d26c884cd0cfdfdd9efc6
Jake Tian <jaketianeduuse@outlook.com>
2023-01-12 19:07:44 +0900

    finalization
    finishing up the actual scenes. improved the object reset script

--- Assets/ObjectReset.cs
+++ Assets/ObjectReset.cs
@@ -1,32 +1,37 @@
 using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ObjectReset : MonoBehaviour
 {
     private bool isPlaced;
     private Transform originalParent;
     private Vector3 originalPosition;
+    public Vector3 originalScale;
     private Quaternion originalRotation;
     // Start is called before the first frame update
-
     private void Start()
     {
         var trans = transform;
+        originalScale= trans.localScale;
         originalParent = trans.parent;
         originalPosition = trans.localPosition;
         originalRotation = trans.localRotation;
+
     }
     public void Reset()
     {
-        isPlaced = true;
 
         // Reset parent and placement of object
         var trans = transform;
         trans.SetParent(originalParent);
+        trans.localScale = originalScale;   
         trans.localPosition = originalPosition;
         trans.localRotation = originalRotation;
+
     }
 
     // Update is called once per frame
